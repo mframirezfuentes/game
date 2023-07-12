8612c2ec5ac96e57f6807c38f0a679bab3656aa3
Fernanda Ramírez <mframirezfuentes@gmail.com>
2023-07-12 19:09:36 -0400

    build colorMode Switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
-
 function App() {
   return (
     <Grid
@@ -11,17 +10,13 @@ function App() {
       }}
     >
       <GridItem area="nav">
-       <NavBar/>
+        <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
 
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
