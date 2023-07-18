03d7cff3fbfbaf56506371f898bf553bd947e970
Fernanda Ramírez <mframirezfuentes@gmail.com>
2023-07-18 15:44:58 -0400

    refactor gameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,10 +12,9 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
-  console.log(gameQuery)
-  return (
-    <>
-      {error && <Text>{error}</Text>}
+
+  if(error) return <Text>{error}</Text>
+  return(
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
@@ -33,7 +32,7 @@ const GameGrid = ({ gameQuery }: Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
+  
   );
 };
 
